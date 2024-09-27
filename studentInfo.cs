538cbd817bdde8792f0a11f777e43c23fbf532c2
SureshGangalmale <sureshgangalmale876@gmail.com>
2024-09-27 10:23:44 +0530

    assignment question inheritance

---
+++ studentInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class person
+    {
+        public string Name="suresh";
+        public int Age=21;
+    }
+    class Student : person
+    {
+         public Student()
+        {
+
+            Console.WriteLine("Name is = " + Name + "\nAge is = "+Age);
+        }
+    }
+    internal class studentInfo
+    {
+        static void Main()
+        {
+            Student s1 = new Student();
+        }
+    }
+}
