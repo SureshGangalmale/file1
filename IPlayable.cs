b6dd2f5cb1e0cee973942694fcaec2d57aa2855f
SureshGangalmale <sureshgangalmale876@gmail.com>
2024-10-01 12:09:02 +0530

    interface assignment example

---
+++ IPlayable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    internal interface IPlayable
+    {
+        void play();
+    }
+    class VideoPlayer:IPlayable
+    {
+        public void play()
+        {
+            Console.WriteLine("Playing Video...");
+        }
+
+    }
+
+    class MusicPlayer:IPlayable
+    {
+        public void play()
+        {
+            Console.WriteLine("Playing Music...");
+        }
+    }
+    internal class demo
+    {
+        static void Main()
+        {
+            IPlayable video=new VideoPlayer();
+            video.play();
+            IPlayable music=new MusicPlayer();
+            music.play();
+        }
+    }
+}
