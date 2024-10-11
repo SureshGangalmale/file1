5306b2122296e6ad48a67ad0ce357794278b1cd7
SureshGangalmale <sureshgangalmale876@gmail.com>
2024-10-11 10:47:33 +0530

    in c#,Delegate programs Single Cast Delegate And Multi Cast Delegate

---
+++ SingleCastDelegate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    public delegate void addDelegate(int a,int b);  
+    public delegate void subDelegate(int a,int b);
+
+    class Delegate1
+    {
+        public void addNumber(int a,int b)
+        {
+            Console.WriteLine("Addition of Two numbers="+(a+b));
+        }
+        public void subNumber(int a,int b)
+        {
+            Console.WriteLine("Subtraction of two Numbers=" + (a - b));
+        }
+    }
+    internal class SingleCastDelegate
+    {
+        static void Main()
+        {
+            Delegate1 delegate1 = new Delegate1();
+            addDelegate ad = new addDelegate(delegate1.addNumber);
+            ad(20, 40);
+
+            subDelegate sd=new subDelegate(delegate1.subNumber);
+            sd(100, 50);
+
+        }
+    }
+}
---
+++ multicastdelegate1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    public delegate void arithmeticOperation(int a, int b);
+
+    public class mydelegate
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Addition is="+(a+b));
+        }
+        public void Sub(int a,int b)
+        {
+            Console.WriteLine("Subtraction is="+(a-b));
+        }
+        public void Mul(int a,int b)
+        {
+            Console.WriteLine("Multiplication is="+(a*b));
+        }
+    }
+    internal class multicastdelegate1
+    {
+        static void Main()
+        {
+           
+
+
+            mydelegate MD = new mydelegate();
+
+            arithmeticOperation ao = new arithmeticOperation(MD.Add);
+            ao += MD.Sub;
+            ao += MD.Mul;
+
+            
+            ao(6, 2);
+        }
+    }
+}
