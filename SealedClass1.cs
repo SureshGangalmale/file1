bed54ba8f13935703f3e13c91a40390e8da85b2c
SureshGangalmale <sureshgangalmale876@gmail.com>
2024-10-18 07:01:08 +0530

    C# Assignment Sealed And Static class

---
+++ SealedClass1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    class one
+    {
+        public void Add()
+        {
+            int a = 40; int b = 20;
+            int c = a + b;
+            Console.WriteLine("Addition is ="+c);
+        }
+
+    }
+    sealed class  Two: one
+    {
+
+        public void Sub()
+        {
+            int a = 40; int b = 20;
+            int c = a - b;
+            Console.WriteLine("Subtraction is ="+c);
+        }
+    }
+    internal class SealedClass1
+    {
+        static void Main()
+        {
+            Two obj1=new Two();
+            obj1.Add();
+            obj1.Sub();
+        }
+    }
+}
---
+++ StaticClass1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    static class CatWorld
+    {
+        public static int NumberOfCats { get; set; }
+        public static int TotalCatNaps { get; set; }
+        static CatWorld()
+        {
+            NumberOfCats = 5;
+        }
+        public static void AddCat()
+        {
+            Console.WriteLine($"Current Cat Count Is:- {NumberOfCats}");
+            NumberOfCats++;
+            Console.WriteLine("\n new Cat Has Joined..");
+        }
+    }
+    internal class StaticClass1
+    {
+        static void Main()
+        {
+            CatWorld.AddCat();
+            CatWorld.AddCat();
+            CatWorld.AddCat();
+
+        }
+    }
+}
