d13b12f1732c3d67b1aece4e34d79aa69d8d54e8
SureshGangalmale <sureshgangalmale876@gmail.com>
2024-10-18 11:06:37 +0530

    Assignment on ref And out parameter

---
+++ out1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    internal class out1
+    {
+        static void InitializeValue(out int number,out string Message)
+        {
+            number = 1;
+            Message = "Welcome to CodeMind Technology";
+        }
+        static void Main()
+        {
+            InitializeValue(out int value, out string str);
+            Console.WriteLine($"{value}\n{str}");
+        }
+    }
+}
---
+++ refIncrementValue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+   
+    internal class refIncrementValue
+    {
+        public static void increment(ref int a)
+        {
+            a++;
+            Console.WriteLine("value is incremented...");
+            Console.WriteLine("value is=" + a);
+        }
+        public static void Main()
+        {
+            int n = 2;
+            increment(ref n);
+            increment(ref n);
+            increment(ref n);
+        }
+    }
+}
